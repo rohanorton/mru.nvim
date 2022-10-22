46152fd6fa0536cb8a35b45b9297820076d0d6af
Rohan Orton <rohan.orton@gmail.com>
2022-10-22 11:50:57 +0200

    Make test pass

--- lua/mru/init.lua
+++ lua/mru/init.lua
@@ -1,9 +1,28 @@
 local M = {}
 
-M.setup = function() end
-M.cleanup = function() end
+local AUGROUP = "mru__group"
+
+local store
+
+M.setup = function()
+  local group = vim.api.nvim_create_augroup(AUGROUP, { clear = true })
+
+  vim.api.nvim_create_autocmd("BufRead", {
+    pattern = "*",
+    callback = function()
+      local file = vim.fn.expand("<afile>")
+      store = file
+    end,
+    group = group,
+  })
+end
+
+M.cleanup = function()
+  vim.api.nvim_del_augroup_by_name(AUGROUP)
+end
+
 M.get = function()
-  return "test_file_3"
+  return store
 end
 
 return M
