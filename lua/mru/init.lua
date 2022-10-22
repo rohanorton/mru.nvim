b080ab63e782b2a5023f356b0700d2be3132b696
Rohan Orton <rohan.orton@gmail.com>
2022-10-22 14:21:18 +0200

    Make test pass

--- lua/mru/init.lua
+++ lua/mru/init.lua
@@ -1,18 +1,21 @@
 local Store = require("mru.store")
+local Path = require("plenary.path")
 
 local M = {}
 
 local AUGROUP = "mru__group"
 
-local store = Store()
+local store
 
 M.setup = function()
+  store = Store()
+
   local group = vim.api.nvim_create_augroup(AUGROUP, { clear = true })
 
   vim.api.nvim_create_autocmd("BufRead", {
     pattern = "*",
     callback = function()
-      local file = vim.fn.expand("<afile>")
+      local file = vim.fn.expand("<afile>:p")
       store.add(file)
     end,
     group = group,
@@ -24,10 +27,19 @@ M.cleanup = function()
 end
 
 M.get = function()
-  return store.get()
+  return Path.new(store.get()):make_relative()
 end
 
 M.list = function()
+  local s = store.list()
+  local res = {}
+  for i, filepath in ipairs(s) do
+    res[i] = Path:new(filepath):make_relative()
+  end
+  return res
+end
+
+M.list_absolute = function()
   return store.list()
 end
 
